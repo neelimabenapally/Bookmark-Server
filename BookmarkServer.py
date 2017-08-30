ed719fd22283ae2e02438e5363bf16353982df76
neelima benapally <neelima.benapally@gmail.com>
2017-08-30 14:19:57 +0100

    Add threadhttpserver class

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -4,8 +4,10 @@
 
 import http.server
 import requests
-import os 
+import os
 from urllib.parse import unquote, parse_qs
+import threading
+from socketserver import ThreadingMixIn
 
 memory = {}
 
@@ -106,8 +108,11 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.wfile.write(
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
+    port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
